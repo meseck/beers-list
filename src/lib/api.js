129baba32c6d7dae2be84d357915ff68d0aed9fe
Fool's Mate <mail@foolsmate.de>
2019-04-26 10:42:17 +0200

    ♻️ Implemented express-http-proxy

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,5 @@
 export const fetchDataFromServer = page => {
-  return fetch('/beers/' + page).then(res => res.json());
+  return fetch('/beers/' + page).then(res =>
+    res.json()
+  );
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const request = require('request');
-const bodyParser = require('body-parser');
+const proxy = require('express-http-proxy');
 const app = express();
 
-app.get('/beers/:page', (req, res) => {
-  const url = 'https://sandbox-api.brewerydb.com/v2';
-  const endpoint = '/beers';
-  const apiKey = '4c036f77b4a036aedb2085ef6a64cc0f';
-  const format = 'json';
-  const pageNumber = req.params.page;
+const url = 'https://sandbox-api.brewerydb.com';
+const endpoint = '/v2/beers';
+const apiKey = '4c036f77b4a036aedb2085ef6a64cc0f';
+const format = 'json';
 
-  const options = {
-    method: 'GET',
-    url: `${url}${endpoint}/?key=${apiKey}&format=${format}&p=${pageNumber}`,
-  };
-
-  return request(options, (err, response, body) => {
-    res.send(body);
-  });
-});
+app.use(
+  '/beers/:page',
+  proxy(url, {
+    proxyReqPathResolver: req => {
+      const pageNumber = req.params.page;
+      const path = `${endpoint}/?key=${apiKey}&format=${format}&p=${pageNumber}`;
+      return path;
+    },
+  })
+);
 
 const server = app.listen(3001, function() {
   console.log('Listening on port ' + server.address().port);
